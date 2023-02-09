217e2ec130a27747084547c5c07cd5f75b8b1418
Lucy Bishop <lucy.bishop@dorsetcouncil.gov.uk>
2023-02-09 14:43:53 +0000

    Bounds management validation added and CRUD pages finished

--- GIFrameworkMap.Data/Data/Models/Bound.cs
+++ GIFrameworkMap.Data/Data/Models/Bound.cs
@@ -1,16 +1,37 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace GIFrameworkMaps.Data.Models
 {
     public class Bound
     {
         public int Id { get; set; }
+
+        [Required]
         [MaxLength(100)]
         public string Name { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
+        [DisplayName ("Bottom left X co-ordinate")]
+        [Range(-20037508.34, 20037508.34)]
         public decimal BottomLeftX { get; set; }
+
+        [Required]
+        [DisplayName("Bottom left Y co-ordinate")]
+        [Range(-20048966.1, 20048966.1)]
         public decimal BottomLeftY { get; set; }
+
+        [Required]
+        [DisplayName("Top right X co-ordinate")]
+        [Range(-20037508.34, 20037508.34)]
         public decimal TopRightX { get; set; }
+
+        [Required]
+        [DisplayName("Top right Y co-ordinate")]
+        [Range(-20048966.1, 20048966.1)]
         public decimal TopRightY { get; set; }
 
     }
