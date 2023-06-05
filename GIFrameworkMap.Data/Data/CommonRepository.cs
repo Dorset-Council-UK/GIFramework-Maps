ccb8ed3d86747253414bfffd0527088f50b5c4c1
Rob Quincey <rpquincey@hotmail.com>
2023-06-05 18:35:07 +0100

    Remove unnecessary usings

--- GIFrameworkMap.Data/Data/CommonRepository.cs
+++ GIFrameworkMap.Data/Data/CommonRepository.cs
@@ -5,16 +5,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.ObjectPool;
-using Npgsql.TypeMapping;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using shortid;
-using System.Reflection.Metadata;
 
 namespace GIFrameworkMaps.Data
 {
