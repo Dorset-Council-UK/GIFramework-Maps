31e3b09ebce5b147edebe83eab7447604dd63920
Rob Quincey <rob.quincey@dorsetcouncil.gov.uk>
2023-09-07 15:02:55 +0100

    Make Civic cookie control (if used) more prominent
    Also rename incorrectly named Civica -> Civic

--- GIFrameworkMaps.Web/Scripts/CookieControls.ts
+++ GIFrameworkMaps.Web/Scripts/CookieControls.ts
@@ -3,7 +3,7 @@ declare var configure_cookie_control: string;
 
 function loadCookieControl() {
     //This allows the use of a cookie control
-    if (configure_cookie_control == "Civica Cookie Control" && typeof CookieControl != "undefined") {
+    if (configure_cookie_control == "Civic Cookie Control" && typeof CookieControl != "undefined") {
         document.getElementById("CookieControlLink").addEventListener("click", CookieControl.open);
     }
 }
