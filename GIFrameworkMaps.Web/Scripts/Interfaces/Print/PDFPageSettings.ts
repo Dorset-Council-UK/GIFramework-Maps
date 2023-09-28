74c6b44cd8a0a723ce6af80caf9181aac7ef6a71
Rob Quincey <109682469+RobQuincey-DC@users.noreply.github.com>
2023-09-28 16:57:45 +0100

    Add legends to prints (#165)
    * Add basic UI elements and initial TS changes for legends
    
    * WIP: Add legends to seperate page
    
    * Create seperate page key
    
    * Minor, unrelated UI tweak to sidebar cards
    
    * WIP: Redesign printing flow to handle legends
    
    * WIP: Add pinned and float left legend options
    
    * Cleanup print code and fix cancel errors
    
    * Fix legend loading issue
    
    * Code cleanup
    
    * Remove unrendered legends from output and fix type
    
    ---------
    
    Co-authored-by: Rob Quincey <rob.quincey@dorsetcouncil.gov.uk>

--- GIFrameworkMaps.Web/Scripts/Interfaces/Print/PDFPageSettings.ts
+++ GIFrameworkMaps.Web/Scripts/Interfaces/Print/PDFPageSettings.ts
@@ -1,13 +1,35 @@
-﻿export interface PDFPageSetting {
+﻿/**
+Defines properties used when generating a PDF
+ */
+export interface PDFPageSetting {
+    /** The font size of the main title if the user requested it. Also the size of the Map Key title if on map*/
     titleFontSize: number;
+    /** The font size of the subtitle and date. Also the size of the individual layer titles on the key*/
     subtitleFontSize: number;
+    /** The font size of the attribution*/
     attributionFontSize: number;
-    pageWidth: number;
-    pageHeight: number;
+    /** The font size of the legend title when it is rendered on a separate page*/
+    standaloneLegendTitleFontSize: number;
+    /** The maximum width a title block can be on the map when in landscape mode */
     maxLandscapeTitleLength: number;
+    /** The maximum width a title block can be on the map when in portrait mode */
     maxPortraitTitleLength: number;
+    /** The maximum width a subtitle block can be on the map when in landscape mode */
     maxLandscapeSubtitleLength: number;
+    /** The maximum width a subtitle block can be on the map when in portrait mode */
     maxPortraitSubtitleLength: number;
+    /** The total width of the page */
+    pageWidth: number;
+    /** The total height of the page */
+    pageHeight: number;
+    /** The maximum width a legend can be when on the map in portrait mode */
+    inlineLegendPortraitMaxWidth?: number;
+    /** The maximum width a legend can be when on the map in landscape mode */
+    inlineLegendLandscapeMaxWidth?: number;
+    /** The wrap_limit to apply to the GetLegendGraphic request when in landscape mode (wrap_limit is non standard and may not work with all services) */
+    landscapeKeyWrapLimit: number;
+    /** The wrap_limit to apply to the GetLegendGraphic request when in portrait mode (wrap_limit is non standard and may not work with all services) */
+    portraitKeyWrapLimit: number;
 }
 
 export type PDFPageSettings = {
