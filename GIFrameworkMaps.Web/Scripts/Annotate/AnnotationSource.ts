1437bdd3e5586efcf5512a98c88fda6c7fe04f0f
Rob Quincey <rob.quincey@dorsetcouncil.gov.uk>
2023-11-08 16:03:48 +0000

    Hotfix update OpenLayers to 8.1.1 dev to fix https://github.com/openlayers/openlayers/issues/15109

--- GIFrameworkMaps.Web/Scripts/Annotate/AnnotationSource.ts
+++ GIFrameworkMaps.Web/Scripts/Annotate/AnnotationSource.ts
@@ -1,8 +1,9 @@
-﻿import Geometry from "ol/geom/Geometry";
+﻿import Feature from "ol/Feature";
+import { Geometry } from "ol/geom";
 import VectorSource from "ol/source/Vector";
 import { Options } from "ol/source/VectorTile";
 
-export default class AnnotationSource extends VectorSource<Geometry> {
+export default class AnnotationSource extends VectorSource<Feature<Geometry>> {
 
     constructor(options: Options = { wrapX: false }) {
         super(options);
