f67664c781519ddeca826484abb01b8d658a36a4
Weolo <matt.mailuk@gmail.com>
2024-03-22 19:06:14 +0000

    Moved and renamed management view models

---
+++ GIFrameworkMap.Data/ViewModels/Management/AnalyticsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using GIFrameworkMaps.Data.Models;
+using System.Collections.Generic;
+
+namespace GIFrameworkMaps.Data.ViewModels.Management
+{
+	public class AnalyticsViewModel
+	{
+		public IList<AnalyticsDefinition> AvailableAnalytics { get; set; } = [];
+		public int VersionID { get; set; }
+	}
+}
---
+++ GIFrameworkMap.Data/ViewModels/Management/LayerSourceOptionEditViewModel.cs
@@ -0,0 +1,10 @@
+﻿using GIFrameworkMaps.Data.Models;
+
+namespace GIFrameworkMaps.Data.ViewModels.Management
+{
+	public class LayerSourceOptionEditViewModel
+	{
+		public LayerSourceOption? LayerSourceOption { get; set; }
+		public LayerSource? LayerSource { get; set; }
+	}
+}
