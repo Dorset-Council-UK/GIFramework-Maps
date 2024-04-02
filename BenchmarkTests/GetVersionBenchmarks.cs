9ff6154011fe7da01e2ba47248bbd9d0cadfd818
Weolo <matt.mailuk@gmail.com>
2024-04-02 17:07:53 +0100

    Version benchmarks

--- BenchmarkTests/GetVersionBenchmarks.cs
+++ BenchmarkTests/GetVersionBenchmarks.cs
@@ -9,13 +9,13 @@ namespace BenchmarkTests
 		private static readonly int versionId = 8;
 
 		[Benchmark(Baseline = true)]
-		public GIFrameworkMaps.Data.Models.Version Original()
+		public GIFrameworkMaps.Data.Models.Version? Original()
 		{
 			return _commonRepository.GetVersionOriginal(versionId);
 		}
 
 		[Benchmark]
-		public async Task<GIFrameworkMaps.Data.Models.Version> GetVersions()
+		public async Task<GIFrameworkMaps.Data.Models.Version?> Version()
 		{
 			return await _commonRepository.GetVersion(versionId);
 		}
