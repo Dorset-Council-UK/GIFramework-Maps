f00983e36520d44943f9a4c02134c50b84fd4383
Weolo <matt.mailuk@gmail.com>
2024-04-03 15:22:23 +0100

    Improved the readability the print repository code

--- GIFrameworkMap.Data/Data/PrintRepository.cs
+++ GIFrameworkMap.Data/Data/PrintRepository.cs
@@ -20,39 +20,36 @@ namespace GIFrameworkMaps.Data
 
         public Models.VersionPrintConfiguration GetPrintConfigurationByVersion(int versionId)
         {
-            if (_context.Versions.Where(v => v.Id == versionId).AsNoTrackingWithIdentityResolution().Any())
+			if (!_context.Versions.Where(v => v.Id == versionId).AsNoTrackingWithIdentityResolution().Any())
+			{
+				throw new KeyNotFoundException($"Version with ID {versionId} does not exist");
+			}
+
+			string cacheKey = "PrintConfigurationByVersion/" + versionId.ToString();
+
+            if (_memoryCache.TryGetValue(cacheKey, out Models.VersionPrintConfiguration? cacheValue))
             {
-                string cacheKey = "PrintConfigurationByVersion/" + versionId.ToString();
-
-                if (_memoryCache.TryGetValue(cacheKey, out Models.VersionPrintConfiguration? cacheValue))
-                {
-                    //using null forgiving operator as if a value is found in the cache it must not be null
-                    return cacheValue!;
-                }
-                else
-                {
-                    var printConfig = _context.VersionPrintConfigurations
-                        .Where(v => v.VersionId == versionId)
-                        .AsNoTrackingWithIdentityResolution()
-                        .Include(v => v.PrintConfiguration)
-                        .FirstOrDefault();
-
-                    //If null get default based on general version (which should always exist)
-                    printConfig ??= _context.VersionPrintConfigurations
-                            .Where(v => v.Version!.Slug == "general")
-                            .AsNoTrackingWithIdentityResolution()
-                            .Include(v => v.PrintConfiguration)
-                            .First();
-
-                    // Cache the results so they can be used next time we call this function.
-                    _memoryCache.Set(cacheKey, printConfig, TimeSpan.FromMinutes(10));
-                    return printConfig;
-                }
+                //using null forgiving operator as if a value is found in the cache it must not be null
+                return cacheValue!;
             }
-            else
-            {
-                throw new KeyNotFoundException($"Version with ID {versionId} does not exist");
-            }
+
+            var printConfig = _context.VersionPrintConfigurations
+                .Where(v => v.VersionId == versionId)
+                .AsNoTrackingWithIdentityResolution()
+                .Include(v => v.PrintConfiguration)
+                .FirstOrDefault();
+
+            //If null get default based on general version (which should always exist)
+            printConfig ??= _context.VersionPrintConfigurations
+                    .Where(v => v.Version!.Slug == "general")
+                    .AsNoTrackingWithIdentityResolution()
+                    .Include(v => v.PrintConfiguration)
+                    .First();
+
+            // Cache the results so they can be used next time we call this function.
+            _memoryCache.Set(cacheKey, printConfig, TimeSpan.FromMinutes(10));
+
+            return printConfig;
         }
     }
 }
