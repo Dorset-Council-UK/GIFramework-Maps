3aa764b61dab6c51fb1406b2568b01010b352bdc
Weolo <matt.mailuk@gmail.com>
2024-04-04 16:29:09 +0100

    Benchamrking management view m

--- BenchmarkTests/ManagementViewModelBenchmarks.cs
+++ BenchmarkTests/ManagementViewModelBenchmarks.cs
@@ -39,17 +39,27 @@ namespace BenchmarkTests
 		}
 
 		[Benchmark]
-		public async Task AsyncProjection()
+		public void Select()
 		{
-			var themesProjection = _context.Themes.OrderBy(o => o.Name).Select(o => new { o.Id, o.Name }).ToListAsync();
-			var boundsProjection = _context.Bounds.OrderBy(o => o.Name).Select(o => new { o.Id, o.Name }).ToListAsync();
-			var welcomeMessagesProjection = _context.WelcomeMessages.OrderBy(o => o.Name).Select(o => new { o.Id, o.Name }).ToListAsync();
-			var toursProjection = _context.TourDetails.OrderBy(o => o.Name).Select(o => new { o.Id, o.Name }).ToListAsync();
+			var themes = _context.Themes
+				.Select(o => new { o.Id, o.Name })
+				.OrderBy(o => o.Name)
+				.ToList();
 
-			var themes = await themesProjection;
-			var bounds = await boundsProjection;
-			var welcomeMessages = await welcomeMessagesProjection;
-			var tours = await toursProjection;
+			var bounds = _context.Bounds
+				.Select(o => new { o.Id, o.Name })
+				.OrderBy(o => o.Name)
+				.ToList();
+
+			var welcomeMessages = _context.WelcomeMessages
+				.Select(o => new { o.Id, o.Name })
+				.OrderBy(o => o.Name)
+				.ToList();
+
+			var tours = _context.TourDetails
+				.Select(o => new { o.Id, o.Name })
+				.OrderBy(o => o.Name)
+				.ToList();
 
 			var availableThemes = new SelectList(themes, "Id", "Name", version.ThemeId);
 			var availableBounds = new SelectList(bounds, "Id", "Name", version.BoundId);
@@ -57,5 +67,33 @@ namespace BenchmarkTests
 			var availableTours = new SelectList(tours, "Id", "Name", version.TourDetailsId);
 		}
 
+		[Benchmark]
+		public async Task SelectAsync()
+		{
+			var themes = await _context.Themes
+				.Select(o => new { o.Id, o.Name })
+				.OrderBy(o => o.Name)
+				.ToListAsync();
+
+			var bounds = await _context.Bounds
+				.Select(o => new { o.Id, o.Name })
+				.OrderBy(o => o.Name)
+				.ToListAsync();
+
+			var welcomeMessages = await _context.WelcomeMessages
+				.Select(o => new { o.Id, o.Name })
+				.OrderBy(o => o.Name)
+				.ToListAsync();
+
+			var tours = await _context.TourDetails
+				.Select(o => new { o.Id, o.Name })
+				.OrderBy(o => o.Name)
+				.ToListAsync();
+
+			var availableThemes = new SelectList(themes, "Id", "Name", version.ThemeId);
+			var availableBounds = new SelectList(bounds, "Id", "Name", version.BoundId);
+			var availableWelcomeMessages = new SelectList(welcomeMessages, "Id", "Name", version.WelcomeMessageId);
+			var availableTours = new SelectList(tours, "Id", "Name", version.TourDetailsId);
+		}
 	}
 }
