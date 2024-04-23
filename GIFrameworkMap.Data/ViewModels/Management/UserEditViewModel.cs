2ec4b7694e9b8ef1db7ba6007bc15e9f7f282e57
weolo <matt.mailuk@gmail.com>
2024-04-23 17:06:09 +0100

    Using select list items for UserEditViewModel

--- GIFrameworkMap.Data/ViewModels/Management/UserEditViewModel.cs
+++ GIFrameworkMap.Data/ViewModels/Management/UserEditViewModel.cs
@@ -1,16 +1,27 @@
-﻿using GIFrameworkMaps.Data.Models;
-using GIFrameworkMaps.Data.Models.Authorization;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 
 namespace GIFrameworkMaps.Data.ViewModels.Management
 {
 	public class UserEditViewModel
 	{
+		protected readonly string _nameSlugSeparator = "__";
+
 		public Microsoft.Graph.Beta.Models.User? User { get; set; }
 		public IList<int> SelectedRoles { get; set; } = [];
-		public IList<ApplicationRole> AvailableRoles { get; set; } = [];
-
 		public IList<int> SelectedVersions { get; set; } = [];
-		public IList<Version> AvailableVersions { get; set; } = [];
+		public IList<SelectListItem> AvailableRoles { get; set; } = [];
+		public IList<SelectListItem> AvailableVersions { get; set; } = [];
+
+		public string CombineNameAndSlug(string name, string slug)
+		{
+			return $"{name}{_nameSlugSeparator}{slug}";
+		}
+
+		public (string Name, string Slug) SplitNameAndSlug(SelectListItem item)
+		{
+			var parts = item.Text.Split(_nameSlugSeparator);
+			return (parts[0], parts[1]);
+		}
 	}
 }
