97eaf1f46827e0ebca56e85fb96107a2efba14fa
weolo <matt.mailuk@gmail.com>
2024-04-23 15:10:17 +0100

    Smal improvement

--- GIFrameworkMap.Data/Data/ManagementRepository.cs
+++ GIFrameworkMap.Data/Data/ManagementRepository.cs
@@ -216,7 +216,7 @@ namespace GIFrameworkMaps.Data
                     return cachedUsers;
                 }
             }
-            List<Microsoft.Graph.Beta.Models.User> allUsers = new();
+            List<Microsoft.Graph.Beta.Models.User> allUsers = [];
             var graphClient = GetGraphClient();
             if (graphClient != null)
             {
@@ -243,7 +243,7 @@ namespace GIFrameworkMaps.Data
                     throw new System.Exception("Graph client could not be initialized");
                 }
             }
-            if (allUsers.Any())
+            if (allUsers.Count != 0)
             {
                 _memoryCache.Set(cachekey, allUsers, System.TimeSpan.FromMinutes(5));
             }
