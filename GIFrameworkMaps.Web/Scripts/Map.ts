68c2d95e1474133d1c489a883659e41e6d75fd67
Rob Quincey <109682469+RobQuincey-DC@users.noreply.github.com>
2024-05-14 11:56:18 +0100

    Add embed mode (#259)
    * Basic CSS changes for embedded context
    
    Includes a hacky means of detecting embed mode request
    
    * Allow scripts to make use of embed mode
    
    - Adds a mode property into the Map instance for use by other scripts
    - Improvements to how sidebar panels are defined
    - Improvement to how attributions collapse in different contexts
    
    * Imrpove search options code
    
    * Tweak how collapsible attributions work

--- GIFrameworkMaps.Web/Scripts/Map.ts
+++ GIFrameworkMaps.Web/Scripts/Map.ts
@@ -50,6 +50,7 @@ export class GIFWMap {
   layerGroups: LayerGroup[];
   sidebars: gifwSidebar.Sidebar[];
   popupOverlay: GIFWPopupOverlay;
+  mode: 'full' | 'embed' = 'full';
   olMap: olMap;
   customControls: (
     | GIFWMousePositionControl
@@ -64,10 +65,12 @@ export class GIFWMap {
     id: string,
     config: VersionViewModel,
     sidebars: gifwSidebar.Sidebar[],
+    mode: 'full' | 'embed'
   ) {
     this.id = id;
     this.config = config;
     this.sidebars = sidebars;
+    this.mode = mode;
     this.delayPermalinkUpdate = debounce(() => {
       document
         .getElementById(this.id)
@@ -104,8 +107,10 @@ export class GIFWMap {
       );
     }
     // set up controls
+  
     const attribution = new olControl.Attribution({
-      collapsible: false,
+      collapsible: true,
+      collapsed: this.mode === 'embed',
     });
     const scaleline = new olControl.ScaleLine({
       units: "metric",
@@ -114,7 +119,7 @@ export class GIFWMap {
       autoHide: false,
       tipLabel: "Reset rotation (Alt-Shift and Drag to rotate)",
     });
-    //mouse position controls. TODO - alow DB setting of initial coord system
+    //add mouse position
     const mousePosition = new GIFWMousePositionControl(
       defaultViewProjection.epsgCode.toString(),
       defaultViewProjection.defaultRenderedDecimalPlaces,
@@ -390,10 +395,12 @@ export class GIFWMap {
       map.getView().fit(reprojectedExtent, { size: mapSize });
     }
     //add attribution size checker and app height variable
-    window.addEventListener("resize", () => {
+    if (this.mode !== "embed") {
+      window.addEventListener("resize", () => {
+        this.checkAttributionSize(map, attribution);
+      });
       this.checkAttributionSize(map, attribution);
-    });
-    this.checkAttributionSize(map, attribution);
+    }
 
     //add drag and drop
     this.addDragAndDropInteraction();
@@ -821,9 +828,8 @@ export class GIFWMap {
     map: olMap,
     attribution: olControl.Attribution,
   ): void {
-    const small = map.getSize()[0] < 600;
-    attribution.setCollapsible(small);
-    attribution.setCollapsed(small);
+      const small = map.getSize()[0] < 600;
+      attribution.setCollapsed(small);
   }
 
   /**
