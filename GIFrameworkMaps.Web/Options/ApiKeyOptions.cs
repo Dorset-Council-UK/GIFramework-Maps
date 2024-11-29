b57735d8078408a044fa156287c7f292b57b92c8
tim-forste <tforste@gmail.com>
2024-11-29 12:28:33 -0800

    Create ApiKeyOptions Object to sotre the ApiKeys (stored in Azure Key Vault)

---
+++ GIFrameworkMaps.Web/Options/ApiKeyOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace GIFrameworkMaps.Web.Options
+{
+	public class ApiKeyOptions
+	{
+		public const string ApiKeys = "ApiKeys";
+		public GoogleOptions Google { get; set; }
+
+		public class GoogleOptions
+		{
+			public string MapsApiKey { get; set; }
+		}
+	}
+}
