7b04b1ee518074481cc8c72180d02f161ab8002d
Rob Quincey <rob.quincey@dorsetcouncil.gov.uk>
2025-12-16 14:30:17 +0000

    Move cookie control into main app script

--- GIFrameworkMaps.Web/Scripts/app.ts
+++ GIFrameworkMaps.Web/Scripts/app.ts
@@ -204,3 +204,20 @@ document.addEventListener("DOMContentLoaded", () => {
       ).style.backgroundColor = "var(--primary-color)";
     });
 });
+
+//Civic cookie control integration
+declare let CookieControl: { open: () => void };
+declare let configure_cookie_control: string;
+
+function loadCookieControl() {
+  //This allows the use of a cookie control
+  if (
+    configure_cookie_control == "Civic Cookie Control" &&
+    typeof CookieControl != "undefined"
+  ) {
+    document
+      .getElementById("CookieControlLink")
+      .addEventListener("click", CookieControl.open);
+  }
+}
+window.loadCookieControl = loadCookieControl;
