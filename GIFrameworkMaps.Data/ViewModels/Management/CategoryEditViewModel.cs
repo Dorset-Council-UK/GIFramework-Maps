ce7eba7bbcf13079fd26170854dc9faa5299dd11
Rob Quincey <109682469+RobQuincey-DC@users.noreply.github.com>
2026-07-30 13:30:00 +0100

    Add sortability to layers in categories and improve efficiency of DB update (#443)

--- GIFrameworkMaps.Data/ViewModels/Management/CategoryEditViewModel.cs
+++ GIFrameworkMaps.Data/ViewModels/Management/CategoryEditViewModel.cs
@@ -14,5 +14,11 @@ namespace GIFrameworkMaps.Data.ViewModels.Management
 		public IList<SelectListItem> AvailableLayers { get; set; } = [];
 		public IList<Version> VersionsCategoryAppearsIn { get; set; } = [];
 
+		public Dictionary<int, int> LayerSortOrders { get; set; } = [];
+
+		/// <summary>
+		/// When true, all layers in this category use SortOrder 0 and sort alphabetically by default.
+		/// </summary>
+		public bool SortAlphabetically { get; set; } = true;
 	}
 }
